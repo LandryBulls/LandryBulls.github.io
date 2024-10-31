c02648f98a62ba552ee3300898ef8242c2a4ba9a
LandryBulls <bulls409@gmail.com>
2024-10-31 16:04:25 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -37,3 +37,10 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+function contentToHtml(text) {
+    return text
+      .split('\n\n')
+      .map(paragraph => `<p>${paragraph}</p>`)
+      .join('')
+  }
