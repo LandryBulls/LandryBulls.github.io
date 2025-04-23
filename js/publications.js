c3b8e5981c4a184e13fad09b60ac15b2282e2b4b
LandryBulls <bulls409@gmail.com>
2025-04-23 12:34:40 -0400

    update typo

--- js/publications.js
+++ js/publications.js
@@ -46,7 +46,7 @@ const publications = {
         {
             title: "Detecting distributed social states from multimodal signals in group conversations",
             authors: "Bulls, L., & Thornton, M. A.",
-            venue: "Society for Affective Science 2025 Conference",
+            venue: "Social and Affective Neuroscience Society 2025 Conference",
             year: 2025,
             links: {
                 PDF: "../files/groupstates_poster.pdf"
